4b55b0ff0ba59eb27d0cc218ac492c27571f7095
eleonora <nebrudko18@gmail.com>
2024-01-01 17:02:47 +0000

    HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from "react-redux";
 import { store } from "./state/storeBLL";
 import AppRedux from "./apps/AppRedux/AppRedux";
@@ -12,11 +12,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <AppRedux demo={false} />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 reportWebVitals();
